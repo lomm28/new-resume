8ae08ca5cd068b91f61b5c8080e5ce482b185abc
Dmytro Danko <newgarage@ukr.net>
2017-06-02 00:21:06 +0300

    root path was changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.sendFile('index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
