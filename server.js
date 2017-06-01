99556039378a1214ecfdf60dfac58de4c815ea2c
Dmytro Danko <newgarage@ukr.net>
2017-06-01 11:20:31 +0300

    changed the port

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ var app = express();
 app.get('/', function(req, res) {
 	res.sendFile('index.html');
 });
-app.listen(hostname);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
