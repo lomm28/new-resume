85dacfff4c0ca2e1612c05536d1ae055457fef90
Dmytro Danko <newgarage@ukr.net>
2017-06-03 22:47:55 +0300

    none

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express();
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
 });
