c23707f9f9f04c0635aed1f7737091e1e8b1718e
Dmytro Danko <newgarage@ukr.net>
2017-06-03 22:54:15 +0300

    nonne

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
@@ -8,4 +8,21 @@ app.get('/', function(req, res) {
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('Express server listening on port', port)
+});*/
+
+'use strict';
+
+const path = require('path');
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.Server(app);
+
+const port = process.env.PORT || 8080;
+
+app.use('/', express.static(path.join(__dirname, 'testheroku')));
+
+server.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}/`);
 });
